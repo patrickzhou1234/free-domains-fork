ab1f4cc44694ae4322057f304d5a36707cc97110
Patrick Zhou <patrickchang44543@gmail.com>
2024-02-06 18:38:00 -0800

    Create biologysimulation.is-an.app.js

---
+++ domains/biologysimulation.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A website that hosts biology simulations',
+  domain: 'is-an.app',
+  subdomain: 'biologysimulation',
+  owner: {
+    email: 'patrickchang44543@gmail.com',
+  },
+  record: {
+    CNAME: ['patrickzhou1234.github.io'],
+  },
+  proxy: false,
+})
